aa7c967a16b3b1bbbb39f50d1cd7effd3517a7b5
Artem Skoropisov <sckoropisow.ar@yandex.ru>
2024-03-22 16:09:44 +0300

    Реализовал создание прямого и реверсивного полинома

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -3,9 +3,12 @@ using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,8 +16,9 @@ namespace CRC_32.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
     private Timer _timer;
+
+    private uint[] Crc32Table;
 
     public MainViewModel() 
     {
@@ -38,11 +42,44 @@ public class MainViewModel : ViewModelBase
         }
     }
 
+    private string _inData;
+    public string InputData
+    {
+        get => _inData;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _inData, value);
+        }
+    }
+
+    private string _outData;
+    public string OutputData
+    {
+        get => _outData;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _outData, value);
+        }
+    }
+
+    private bool _isReverse;
+    public bool IsReverse
+    {
+        get => _isReverse;
+        set => this.RaiseAndSetIfChanged(ref _isReverse, value);
+    }
+
     public void Generate()
     {
-        OpenOrCreateFile();
+        if (!string.IsNullOrEmpty(Polynom))
+        {
+            CreateTable();
 
+            byte[] data = Encoding.UTF8.GetBytes(InputData);
+            uint crc32 = CalculateCRC32(data);
 
+            OutputData = crc32.ToString("X");
+        }
     }
 
     private async void OpenOrCreateFile()
@@ -84,16 +121,76 @@ public class MainViewModel : ViewModelBase
         }
     }
 
+    public uint BitsToUInt(bool[] bits)
+    {
+        uint result = 0;
+        for (int i = 0; i < bits.Length; i++)
+        {
+            if (bits[i])
+            {
+                result |= (uint)(1 << i); // Устанавливаем i-тый бит в единицу
+            }
+        }
+        return result;
+    }
+
+    private void CreateTable()
+    {
+        uint polynom = 0;
+
+        //string test = "00000100110000010001110110110111";
+        //string tesn = "11101101101110001000001100100000";
+
+        if (IsReverse)
+        {
+            var array = PolyValue.ToArray();
+            Array.Reverse(array);
+            polynom = BitsToUInt(array);
+        }
+        else polynom = BitsToUInt(PolyValue.ToArray());
+
+        Crc32Table = new uint[256];
+
+        for (uint i = 0; i < 256; i++)
+        {
+            uint crc = i;
+            for (int j = 8; j > 0; j--)
+            {
+                if ((crc & 1) == 1)
+                {
+                    crc = (crc >> 1) ^ polynom;
+                }
+                else
+                {
+                    crc >>= 1;
+                }
+            }
+            Crc32Table[i] = crc;
+        }
+    }
+
+    public uint CalculateCRC32(byte[] data)
+    {
+        uint crc = 0xFFFFFFFF;
+        foreach (byte b in data)
+        {
+            byte index = (byte)(((crc) & 0xFF) ^ b);
+            crc = (crc >> 8) ^ Crc32Table[index];
+        }
+        return ~crc;
+    }
+
     private void ChagePolynom(object? state)
     {
         Polynom = string.Empty;
 
         for (int i = 31; i >= 0; i--)
         {
-            if (PolyValue[i]) Polynom += "X^" + (i + 1).ToString() + "+";
+            if (PolyValue[i])
+            {
+                if (i == 0) Polynom += "1"; 
+                else Polynom += "X^" + (i).ToString() + "+";
+            }
         }
-
-        //if (Polynom.Length > 0 ) Polynom = Polynom.TrimEnd('+');
-        if (Polynom.Length > 0) Polynom += "1"; 
     }
 }
